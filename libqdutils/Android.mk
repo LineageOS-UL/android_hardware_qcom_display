7261fbe17f5ee41c16e66c28448d9005923d9ca0
TARKZiM <tom8476oo@gmail.com>
2023-11-27 01:00:30 +0800

    display: Fix build errors on U

--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -5,9 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := libqdutils
 LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_TAGS             := optional
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libui libbinder libqservice libhardware libutils
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdutils\" -Wno-float-conversion
-LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
+LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers libutils_headers
 LOCAL_SRC_FILES               := profiler.cpp mdp_version.cpp \
                                  idle_invalidator.cpp \
                                  comptype.cpp qd_utils.cpp \
@@ -22,14 +22,15 @@ LOCAL_HEADER_LIBRARIES          := display_headers generated_kernel_headers
 LOCAL_SRC_FILES                 := display_config.cpp
 LOCAL_CFLAGS                    := $(common_flags)
 LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayConfig\"
+LOCAL_VENDOR_MODULE             := true
 LOCAL_MODULE_TAGS               := optional
 LOCAL_MODULE                    := libdisplayconfig
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SHARED_LIBRARIES          := liblog libcutils
-LOCAL_HEADER_LIBRARIES          := display_headers generated_kernel_headers
+LOCAL_SHARED_LIBRARIES          := liblog libcutils libhardware libutils
+LOCAL_HEADER_LIBRARIES          := display_headers generated_kernel_headers libutils_headers
 LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
 LOCAL_CFLAGS                    := $(common_flags)
 LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
@@ -41,9 +42,9 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SHARED_LIBRARIES          := liblog libcutils
+LOCAL_SHARED_LIBRARIES          := liblog libcutils libhardware libutils
 LOCAL_C_INCLUDES                := $(common_includes)
-LOCAL_HEADER_LIBRARIES          := display_headers generated_kernel_headers
+LOCAL_HEADER_LIBRARIES          := display_headers generated_kernel_headers libutils_headers
 LOCAL_SRC_FILES                 := qdMetaData.cpp qd_utils.cpp
 LOCAL_CFLAGS                    := $(common_flags) -Wno-sign-conversion
 LOCAL_CFLAGS                    += -DLOG_TAG=\"DisplayMetaData\"
